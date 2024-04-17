8eceef42c2112ab40e826c4e3ae085f884f6bc7e
Gerardo Avila <40608524+JerryAvila06@users.noreply.github.com>
2024-04-17 16:27:40 -0600

    Update and rename project.v to tt_um_PWM_Sine_UART.v

--- src/tt_um_PWM_Sine_UART.v
+++ src/tt_um_PWM_Sine_UART.v
@@ -17,8 +17,9 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    module PWM_Sine_UART() c1 (.clk1(ui_in[7]) , rst(ui_in[6]) , sw_0(ui_in[5]) , sw_1(ui_in[4]) , uart_rxd(ui_in[0]) , uart_txd(uo_out[0]) , pwm_out(uo_out[2]));
+    assign ui_out = 8'00000101;
+    assign uio_eo = 8'b00000000;
+    assign uio_out = 8'b00000000;
 
 endmodule
