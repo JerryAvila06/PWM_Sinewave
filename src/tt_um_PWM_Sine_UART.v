8699f5981825fbc30ee97e4e1569a7bef96f07c4
Gerardo Avila <40608524+JerryAvila06@users.noreply.github.com>
2024-04-18 11:23:20 -0600

    Update tt_um_PWM_Sine_UART.v

--- src/tt_um_PWM_Sine_UART.v
+++ src/tt_um_PWM_Sine_UART.v
@@ -25,7 +25,7 @@ module tt_um_PWM_Sine_UART (
     assign uart_tx = uo_out[0];
     assign pwm_outx = uo_out[2];
     
-    assign ui_out = 8'00000101;
+    assign ui_out = 8'b00000101;
     assign uio_oe = 8'b00000000;
     assign uio_out = 8'b00000000;
     
